2daf841a122cfceaca3f0e23e2f0d2bdaedff44a
tdubsxc <trdubs90@gmail.com>
2023-01-18 17:16:32 -0500

    Added state to track the last user action.

--- app.js
+++ app.js
@@ -14,25 +14,48 @@ window.addEventListener("load", () => {
 window.addEventListener("click", (e) => {
   if (e.target.matches("#clear")) {
     calculator.clear();
+    setLastAction(actions.clear);
   }
 
   if (e.target.matches("#delete") || e.target.matches("[name=backspace]")) {
     calculator.removeDigit();
+    setLastAction(actions.delete);
   }
 
   if (e.target.matches("#equals")) {
     calculator.evaluate();
+    setLastAction(actions.equals);
   }
 
   if (e.target.matches(".number")) {
+    if (lastActionState === actions.equals) {
+      calculator.clear();
+    }
+
     calculator.addDigit(e.target.textContent);
+    setLastAction(actions.number);
   }
 
   if (e.target.matches(".operation")) {
     calculator.selectOperation(e.target.textContent);
+    setLastAction(actions.operation);
   }
 
   if (e.target.matches("#plus-minus")) {
     calculator.signChange();
+    setLastAction(actions.plusMinus);
   }
 });
+
+function setLastAction(val) {
+  lastActionState = val;
+}
+
+const actions = {
+  clear: "clear",
+  number: "number",
+  delete: "delete",
+  operation: "operation",
+  equals: "equals",
+  plusMinus: "plusMinus",
+};
